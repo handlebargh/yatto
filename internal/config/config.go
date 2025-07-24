01fd98a7617f736d322f713857a15a6a39d08936
Benedikt Zumtobel <benedikt@zumtobel.dev>
2025-07-24 03:31:29 +0200

    docs: add license header and doc string

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,23 @@
+// Copyright 2025 handlebargh
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package config
 
 import (
@@ -8,6 +28,9 @@ import (
 	"github.com/spf13/viper"
 )
 
+// CreateConfigFile reads the Viper config file, and if it is not found,
+// creates the necessary configuration directory under the given home path
+// and writes a new default config file. It panics on any unrecoverable error.
 func CreateConfigFile(home string) {
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
